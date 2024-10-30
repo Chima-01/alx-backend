f7ddd9074bd75fa9ecb4c3680a91f127c4ce5223
chima-01 <divinechima76@gmail.com>
2024-10-30 17:35:46 +0100

    implement lifo cache system

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+    Aclass LIFOCache that inherits from
+    BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    class to implement lifocaching system
+    """
+
+    def put(self, key, item):
+        """add to cache system
+        """
+        cache = self.cache_data
+        if key and item:
+            cache[key] = item
+        if len(cache) > BaseCaching.MAX_ITEMS:
+            key = list(cache.keys())[-2]
+            del cache[key]
+            print(f"DISCARD: {key}")
+
+    def get(self, get):
+        """get item from cache_data
+        """
+        return self.cache_data.get(key, "None")
