504c327c7e303ed89b78f9c26c5c45a167626d83
chima-01 <divinechima76@gmail.com>
2024-10-30 12:19:47 +0100

    implement a fifo cache system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+A class FIFOCache that inherits from
+BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+        a fifocahe system
+    """
+
+    def __init__(self):
+        """ initialize class
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ add item to cache
+        """
+        cache_data = self.cache_data
+        if key and item:
+            cache_data.update({key: item})
+        if len(cache_data) > super().MAX_ITEMS:
+            first = next(iter(cache_data))
+            del cache_data[first]
+            print(f"DISCARD: {first}")
+
+    def get(self, key):
+        """ get value by key
+        """
+        return self.cache.data.get(key)
