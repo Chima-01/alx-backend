35886c8a41c30c8803e64a796474ca675d74b81c
chima-01 <divinechima76@gmail.com>
2024-10-30 17:39:16 +0100

    made a change

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -10,6 +10,8 @@ class LIFOCache(BaseCaching):
     """
     class to implement lifocaching system
     """
+    def __init__(self):
+        super().__init__
 
     def put(self, key, item):
         """add to cache system
@@ -17,10 +19,10 @@ class LIFOCache(BaseCaching):
         cache = self.cache_data
         if key and item:
             cache[key] = item
-        if len(cache) > BaseCaching.MAX_ITEMS:
-            key = list(cache.keys())[-2]
-            del cache[key]
-            print(f"DISCARD: {key}")
+            if len(cache) > BaseCaching.MAX_ITEMS:
+                key = list(cache.keys())[-2]
+                del cache[key]
+                print(f"DISCARD: {key}")
 
     def get(self, get):
         """get item from cache_data
