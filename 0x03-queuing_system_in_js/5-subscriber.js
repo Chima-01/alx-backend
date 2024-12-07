965fe043c0b64ae4d3ee8462ddc6bc581e9eadb1
chima-01 <divinechima76@gmail.com>
2024-12-07 03:47:41 +0100

    using redis subscribe and publish

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -9,3 +9,13 @@ client.on('connect', () => {
 client.on('error', (err) => {
   console.log(`Redis client not connected to the server: ${err}`);
 });
+
+client.subscribe('ALXchannel');
+
+client.on('message', (channel, message) => {
+    console.log(`${message}`);
+    if (message === 'KILL_SERVER') {
+        client.unsubscribe();
+        client.quit();
+    }
+});
